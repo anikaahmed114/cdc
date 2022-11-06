0d7a834ae2d13cf5aa1f034157a4f2e17ccf7088
Anika Ahmed <aahm@unc.edu>
2022-11-06 01:01:24 -0400

    progress

---
+++ my-app/src/App.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from "./components/KeyPadComponent";
+
+class App extends Component {
+  constructor(){
+    super();
+
+  this.state = {
+    result:""
+  }
+  }
+
+render() {
+  return(
+    <div>
+      <div className="calculator-body">
+        <h1> Simple Calculator</h1>
+        <ResultComponent result = {this.state.result}/>
+        <KeyPadComponent onClick = {this.onClick}/>
+      </div>
+    </div>
+  );
+}
+
+
+calculate = () => {
+  try {
+    this.setState({
+      // eslint-disable-next-line
+      result: (eval(this.state.result) || "" ) + ""
+    })
+  } catch (e) {
+    this.setState({
+      result: "error"
+    })
+  }
+};
+
+reset = () => {
+  this.setState({
+    result: ""
+  })
+};
+
+backspace = () => {
+  this.setState({
+    result: this.state.result.slice(0, -1)
+  })
+};
+
+onClick = button => {
+
+  if (button === "="){
+    this.calculate()
+  }
+
+  else if(button === "C"){
+    this.reset()
+  }
+  else if(button === "CE"){
+    this.backspace()
+  }
+  else(
+    this.setState({
+      result: this.state.result + button
+    })
+  )
+}
+}
+  
+export default App
